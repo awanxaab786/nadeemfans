d07822aeef55c12d39580f6ae426ae3aea1d51b9
awanxaab786 <moinawan7788@gmail.com>
2017-03-14 10:52:03 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 ﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><title>
 Ã¢â„¢Â¥ GHANCHI BOTTERX Ã¢â„¢Â¥
-</title><link rel="stylesheet" type="text/css" href="http://flambon.xtgem.com/fmb/interior.css" media="all,handheld"/><link rel="shortcut icon" href="">
+</title><link rel="stylesheet" type="text/css" href="http://junkiez.wen.ru/junkiez/grey.css" media="all,handheld"/><link rel="shortcut icon" href="">
 <script type="text/javascript" src="http://wap4dollar.com/ad/pops/?id=a28e6fl34q"></script>
 
 
